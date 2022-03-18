7cdd8aa548c4e8998f03d823f5979799cf8d7b6b
Youri Gruiters <i-youri-i@hotmail.com>
2022-03-18 13:27:50 +0100

    onnodige bestanden verwijderd

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Heading = ({ content }) => {
+  const alert2 = (data) => {
+    alert(data);
+  };
+
   return (
     <div>
       <h1>{content}</h1>
+      <button onClick={() => alert2("doei")}>Klik hier</button>
     </div>
   );
 };
--- src/test.js
+++
@@ -1,3 +0,0 @@
-function tellen(cijfer1, cijfer2) {
-  return cijfer1 + cijfer2;
-}
