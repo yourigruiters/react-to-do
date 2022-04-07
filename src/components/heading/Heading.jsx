afd815f9017aafd289d6095917718a729651c1b9
Youri Gruiters <56152938+yourigruiters@users.noreply.github.com>
2022-04-07 08:39:58 +0200

    test (#1)

--- src/components/heading/Heading.jsx
+++ src/components/heading/Heading.jsx
@@ -1,16 +1,9 @@
 import React, { useState, useEffect } from "react";
 
 const Heading = ({ beginNaam }) => {
-  const [naam, setNaam] = useState(0);
-
-  useEffect(() => {
-    setNaam(beginNaam);
-  }, []);
-
   return (
     <div>
-      <h1>{naam}</h1>
-      <button onClick={() => setNaam(naam + 1)}>Klik hier</button>
+      <h1>Youri Gruiters</h1>
     </div>
   );
 };
